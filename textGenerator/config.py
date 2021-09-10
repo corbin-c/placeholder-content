980fc5434db17e892f6ec76d444096902e7d366a
Clément Corbin <43074339+corbin-c@users.noreply.github.com>
2021-09-10 09:26:45 +0200

    Update config.py

--- textGenerator/config.py
+++ textGenerator/config.py
@@ -4,5 +4,5 @@ LANG = "fr"
 CATEGORY_PREFIX = "Catégorie"
 SOURCES = ["Économie"]
 LIMIT = 2
-USER_AGENT = "Placeholder/1.0"
+USER_AGENT = "RandomGenerator/1.0 +https://github.com/corbin-c/random-generator"
 TEXT_LIMIT = "À propos de cette édition électronique"
